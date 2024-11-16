1f5881188bae9e8c2ca8dc554b593288143be411
Sasazaki <alvero703@gmail.com>
2024-11-16 17:22:22 +0800

    Update admin_dashboard.php
    added sidebar on the admin dashboard

--- biochem/admin_dashboard.php
+++ biochem/admin_dashboard.php
@@ -39,73 +39,109 @@ $total_companies = $conn->query("SELECT COUNT(*) as count FROM companies")->fetc
         .sidebar {
             height: 100vh;
             position: fixed;
+            padding: 20px;
             top: 0;
             left: 0;
-            width: 250px;
-            background-color: #333;
+            width: 300px;
+            background-color: #ffffff;
             color: white;
+            flex-grow: 1;
         }
         .sidebar a {
-            color: white;
+            color: black;
             text-decoration: none;
             display: block;
+            border-radius: 4%;
             padding: 10px;
+            margin: 20px;
+            font-size: 15pt;
+            width: 90%;
+        }
+        .sidebarButtonContainer img {
+            width: 30px;  
+            height: auto; 
+        }
+        .logoDiv img {
+            width: 260px;  
+            height: auto; 
         }
         .sidebar a:hover {
-            background-color: #555;
+            background-color: #46aeff;
+            color: white ;
+            width: 90%;
+        }
+        .sidebarButtonContainer{
+            margin-top: 30px;
         }
         .content {
-            margin-left: 250px;
-            padding: 20px;
+            padding: 100px;
+            background-color: #f4f4f4;
+            flex-grow: 2;
+            margin-left: 320px;
+            height: 1000px;
         }
+        
+       
     </style>
 </head>
 <body>
-    <button id="sidebarToggle" class="btn btn-primary">Toggle Sidebar</button>
-    <div class="sidebar">
-        <a href="user-management.php">User Management</a>
-        <a href="user-logs.php">User Logs</a>
-        <a href="backup-records.php">Backup Records</a>
-        <a href="restore-records.php">Restore Records</a>
-        <a href="logout.php">Logout</a>
-    </div>
-    <div class="content">
-        <h1>Welcome, <?php echo $user['first_name'] . ' ' . $user['last_name']; ?></h1>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Total Employees</h5>
-                        <p class="card-text"><?php echo $total_employees; ?></p>
+        <div class="sidebar">
+            <div class="logoDiv">
+                <img src="logo.jpg"/>
+            </div>
+            <div class="sidebarButtonContainer">
+                <a href="user-management.php">
+                <img src="userManagement.png"/>  User Management</a>
+                <a href="user-logs.php">
+                <img src="userLog.png"/>  User Logs</a>
+                <a href="backup-records.php">
+                <img src="backup.png"/>  Backup Records</a>
+                <a href="restore-records.php">
+                <img src="restore.png"/>  Restore Records</a>
+                <a href="logout.php">
+                <img src="logout.png"/>  Logout</a>
+            </div>
+            
+        </div>
+        <div class="content">
+            <h1>Welcome, <?php echo $user['first_name'] . ' ' . $user['last_name']; ?></h1>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Employees</h5>
+                            <p class="card-text"><?php echo $total_employees; ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Pending Invoices</h5>
+                            <p class="card-text"><?php echo $pending_invoices; ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Registered Companies</h5>
+                            <p class="card-text"><?php echo $total_companies; ?></p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Pending Invoices</h5>
-                        <p class="card-text"><?php echo $pending_invoices; ?></p>
-                    </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="company_registration.php" class="btn btn-primary mb-3 ml-2">Company Registration</a>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Registered Companies</h5>
-                        <p class="card-text"><?php echo $total_companies; ?></p>
-                    </div>
+                <div class="col-md-6">
+                    <a href="company_rep_management.php" class="btn btn-primary mb-3 ml-2">Manage Company Representatives</a>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6">
-                <a href="company_registration.php" class="btn btn-primary mb-3 ml-2">Company Registration</a>
-            </div>
-            <div class="col-md-6">
-                <a href="company_rep_management.php" class="btn btn-primary mb-3 ml-2">Manage Company Representatives</a>
-            </div>
-        </div>
     </div>
+    
     <script>
 
 </script>
